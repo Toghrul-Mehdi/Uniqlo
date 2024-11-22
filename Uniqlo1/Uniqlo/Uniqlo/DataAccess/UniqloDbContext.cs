eaac3403454be2c4db2d185a229d58911d637d28
Toghrul-Mehdi <togrulcm-bp215@code.edu.az>
2024-11-22 12:46:54 +0400

    Uniqlo

---
+++ Uniqlo1/Uniqlo/Uniqlo/DataAccess/UniqloDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Uniqlo.Models;
+
+namespace Uniqlo.DataAccess
+{
+    public class UniqloDbContext : DbContext
+    {
+        public DbSet<Slider> Sliders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=Uniqlo;Trusted_Connection=True;TrustServerCertificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- Uniqlo1/Uniqlo/Uniqlo/Program.cs
+++ Uniqlo1/Uniqlo/Uniqlo/Program.cs
@@ -7,6 +7,10 @@ namespace Uniqlo
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllersWithViews();
             var app = builder.Build();
+            app.UseStaticFiles();
+            app.MapControllerRoute(name: "areas",
+            pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
             app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
             app.UseStaticFiles();
 
